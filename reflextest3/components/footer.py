8840ad76eaee13091e290c52c2d6bb7aebff4c1b
Willians J. Sequera <wjsequera@gmail.com>
2025-04-28 16:45:56 -0400

    Iconos ChangesV02

--- reflextest3/components/footer.py
+++ reflextest3/components/footer.py
@@ -2,12 +2,13 @@ import reflex as rx
 import datetime
 from reflextest3.stilos.stilos import Size as Size
 from reflextest3.stilos.colors import TextColor as TextColor
+import reflextest3.constants as const
 
 def footer()-> rx.Component:
     return rx.vstack(
                 rx.image(src="publicidadScada.png", width="80px", height="80px", margin_bottom="0px"),
                 rx.link(f"2020-{datetime.date.today().year} wjsequera@gmail.com. python develpoer practice",
-                        href="https://mouredev.com",
+                        href=const.WJSSEQUERA_TWICH,
                         is_external=True,margin_top="0px"
                         ),
                 rx.text("2025 wjsequera@gmail.com. python and reflex ptractice",
