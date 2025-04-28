5dcd4065b851d48d7a6f242f7a9ba862ca4b182b
Willians J. Sequera <wjsequera@gmail.com>
2025-04-28 16:56:04 -0400

    iconosChangeAndLinkFooter01

--- reflextest3/components/footer.py
+++ reflextest3/components/footer.py
@@ -7,7 +7,7 @@ import reflextest3.constants as const
 def footer()-> rx.Component:
     return rx.vstack(
                 rx.image(src="publicidadScada.png", width="80px", height="80px", margin_bottom="0px"),
-                rx.link(f"2020-{datetime.date.today().year} wjsequera@gmail.com. python develpoer practice",
+                rx.link(f"2020-{datetime.date.today().year} wjsequera@gmail.com. python developer practice",
                         href=const.WJSSEQUERA_TWICH,
                         is_external=True,margin_top="0px"
                         ),
